12ddfc02ccdeda4fc920f32abe44b7066c7f795e
Aboussou Daniel-Eric <aboussoudaniel@gmail.com>
2023-04-23 21:52:55 +0000

    amplify auth et apmlify api configurer

--- UserApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ UserApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,29 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "userapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "userappee848e79ee848e79": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "userappee848e79ee848e79PostAuthentication": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- UserApp/metro.config.js
+++ UserApp/metro.config.js
@@ -14,6 +14,7 @@ module.exports = {
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
+         resolver: null,
       },
     }),
   },
